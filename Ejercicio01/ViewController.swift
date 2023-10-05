7000481e58d298ca3af4e576812cbb18c87e6ab9
albertocanoroca <alberto.canoroca@cieep.com>
2023-10-05 14:29:29 +0200

    ee

--- Ejercicio01/ViewController.swift
+++ Ejercicio01/ViewController.swift
@@ -12,8 +12,49 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBOutlet weak var contadorUsu: UILabel!
+    @IBOutlet weak var contadorComp: UILabel!
+    @IBOutlet weak var selecComp: UIImageView!
+    
+    var elecUsu:String = "";
+    var elecComp:Int = 0;
+    
+    public func contador() -> Int{
+        elecComp = Int.random(in: 1..<4);
+       
+        return elecComp;
     }
 
-
+    
+    
+    @IBAction func piedra(_ sender: Any) {
+        contador()
+        if elecComp == 1 {
+            <#code#>
+        }
+        
+    }
+    
+    @IBAction func papel(_ sender: Any) {
+        contador()
+    }
+    
+    
+    @IBAction func tijeras(_ sender: Any) {
+        contador()
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
